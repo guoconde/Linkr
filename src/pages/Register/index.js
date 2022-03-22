a0d00f0102ec8da2afd34ffb13845ed9060f56df
Rui Neto <ruineto11@gmail.com>
2022-03-22 15:39:27 -0300

    configurations of controlled inputs

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { AuthContainer, SloganSide, Logo, Slogan, FormSide } from "../../components/AuthScreenComponents"
 import { Form, Input, Button, StyledLink } from "../../components/FormComponents";
 
 export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [picture, setPicture] = useState('');
+  const [isLoading, setIsloading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <AuthContainer>
       <SloganSide>
@@ -10,13 +21,42 @@ export default function Register() {
       </SloganSide>
 
       <FormSide>
-        <Form>
-          <Input placeholder="e-mail"></Input>
-          <Input placeholder="password"></Input>
-          <Input placeholder="username"></Input>
-          <Input placeholder="picture url"></Input>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            placeholder="e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="picture url"
+            onChange={(e) => setPicture(e.target.value)}
+            value={picture}
+          />
 
-          <Button>Sign Up</Button>
+          <Button type="submit">
+            {isLoading ?
+              "Loading..."
+              :
+              "Sign Up"
+            }
+          </Button>
           <StyledLink to="/">Switch back to log in</StyledLink>
         </Form>
       </FormSide>
