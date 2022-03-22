97d5dca77d266a219866a35e7828d93b348cebb3
Kayque <lkcoelho76@gmail.com>
2022-03-22 11:45:57 -0300

    fix: change header scope

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import ProfilePicture from "../../components/profilePicture";
 
 import { Container, DownArrow, Logout, Title, UserIcon } from "./style";
 
 export default function Header() {
   const [toggleLogout, setToggleLogout] = useState(false);
+  const { pathname } = useLocation();
+
+  if (pathname === "/login" || pathname === "/register") {
+    return null;
+  }
 
   return (
     <Container>
