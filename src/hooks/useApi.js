699614b376177a488b886a696a9588485e4e5428
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-03-24 20:05:35 -0300

    chore: treat conflicts

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,8 +1,5 @@
 import AuthApi from "../services/AuthApi";
-<<<<<<< HEAD
 import FeedApi from "../services/FeedApi";
-=======
->>>>>>> df9760e821c487f9d5401b1d6aa129ef31163049
 import HashtagsApi from "../services/hashtagsApi";
 import PostsApi from "../services/postsApi";
 import UserApi from "../services/userApi";
@@ -12,10 +9,7 @@ export default function useApi() {
         auth: new AuthApi(),
         user: new UserApi(),
         posts: new PostsApi(),
-<<<<<<< HEAD
         feed: new FeedApi(),
-=======
->>>>>>> df9760e821c487f9d5401b1d6aa129ef31163049
         hashtags: new HashtagsApi()
     };
 } 
